6056331b7c686de1230db7dfc64ffc139bc24286
Ariel Pchara <ariel.pchara@ezcommerce.com.br>
2017-02-09 16:53:14 -0200

    icons

--- app/index.js
+++ app/index.js
@@ -1,8 +1,19 @@
 const express = require('express'),
+    nunjucks = require('nunjucks'),
+    path = require('path'),
     app = express();
 
 require('dotenv').config();
 
+nunjucks.configure(path.resolve(__dirname, 'views'), {
+    autoescape: true,
+    express: app
+});
+
 app.use(express.static(__dirname + '/public'));
 
-app.listen(process.env.PORT);
+app.get('/', (req, res) => {
+    res.render('index.html');
+});
+
+app.listen(process.env.PORT, () => console.log('App started'));
